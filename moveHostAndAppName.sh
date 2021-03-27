9af72a239aef91f57b22d61a2f7d1f5ac6a39001
Rohit Kapoor <rvkapoor@tekmonks.com>
2021-03-27 09:28:15 +0000

    fix host name and app name

--- moveHostAndAppName.sh
+++ moveHostAndAppName.sh
@@ -3,6 +3,6 @@
 NEW_HOST=$1
 NEW_APP=$2
 
-grep $NEW_HOST -l -I -r . | xargs sed -i 's/$NEW_HOST/$NEW_HOST/g'
+grep 157.230.65.205 -l -I -r . | xargs sed -i 's/157.230.65.205/$NEW_HOST/g'
 
-grep 'apps/tekmonks' -l -I -r . | xargs sed -i 's=apps/tekmonks=apps/tekmonks=g'
+grep 'apps/tekmonks' -l -I -r . | xargs sed -i 's=apps/tekmonks=$NEW_APP=g'
