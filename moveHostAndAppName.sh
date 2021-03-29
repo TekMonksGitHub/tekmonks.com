7db64561b3f31bbc394ae0aa9b040956c27a41f7
Rohit Kapoor <rvkapoor@tekmonks.com>
2021-03-29 09:44:35 +0000

    copy index_monkshu to monkshu/frontend

--- moveHostAndAppName.sh
+++ moveHostAndAppName.sh
@@ -3,6 +3,10 @@
 NEW_HOST=$1
 NEW_APP=$2
 
-grep 157.230.65.205 -l -I -r . | xargs sed -i 's/157.230.65.205/$NEW_HOST/g'
+grep 157.230.65.205 -l -I -r . | xargs sed -i 's/157.230.65.205/'$NEW_HOST'/g'
 
-grep 'apps/tekmonks' -l -I -r . | xargs sed -i 's=apps/tekmonks=$NEW_APP=g'
+grep 'apps/tekmonks' -l -I -r . | xargs sed -i 's=apps/tekmonks='$NEW_APP'=g'
+
+cp frontend/$NEW_APP/index_monkshu.html ../monkshu/frontend/index.html
+
+cp -R frontend/$NEW_APP/img ../monkshu/frontend/img
