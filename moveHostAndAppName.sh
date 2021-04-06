020d77bc4384272079e0217fb3adbadb0ff6cf31
Arpit Jain <abs.jain16@gmail.com>
2021-04-06 12:41:52 +0530

    Update host entry in script

--- moveHostAndAppName.sh
+++ moveHostAndAppName.sh
@@ -3,7 +3,7 @@
 NEW_HOST=$1
 NEW_APP=$2
 
-grep 157.230.65.205 -l -I -r . | xargs sed -i 's/157.230.65.205/'$NEW_HOST'/g'
+grep tekmonks.com -l -I -r . | xargs sed -i 's/tekmonks.com/'$NEW_HOST'/g'
 
 grep 'apps/tekmonks' -l -I -r . | xargs sed -i 's=apps/tekmonks='$NEW_APP'=g'
 
