7c72729a9d4b8ade1d39bbd8d286545a171081c4
Jezrel Dave Sondia <jezrel.sondia@tekmonks.com>
2023-09-26 09:51:33 +0800

    edit function working for blogs

---
+++ backend/apps/tekmonks/apis/updateBlog.js
@@ -0,0 +1,90 @@
+const API_CONSTANTS = require(`${__dirname}/lib/constants.js`);
+const path = require("path");
+const fs = require("fs");
+const util = require("util");
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+exports.doService = async jsonReq => {
+  if (!validateRequest(jsonReq)) {
+    return { error: "Validation failure." };
+  }
+
+  const title = jsonReq.title;
+  const blog = jsonReq.blog;
+
+  if (!title || !blog) {
+    return { error: "Title and blog content are required." };
+  }
+
+  const filePath = findFileByTitle(title);
+
+  if (!filePath) {
+    return { error: `File with title "${title}" not found.` };
+  }
+
+  try {
+    await updateFileContent(filePath, blog);
+    return { message: `File with title "${title}" updated successfully.`, status: true };
+  } catch (error) {
+    return { error: "Failed to update file content." };
+  }
+};
+
+function validateRequest(jsonReq) {
+  return jsonReq && jsonReq.title && jsonReq.blog;
+}
+
+function findFileByTitle(title) {
+  const folderPath = `${API_CONSTANTS.CMS_ROOT}/blogs.md`; // Adjust the folder path as needed
+
+  function traverseFolder(currentPath) {
+    const files = fs.readdirSync(currentPath);
+
+    for (const file of files) {
+      const filePath = path.join(currentPath, file);
+      const stat = fs.statSync(filePath);
+
+      if (stat.isFile() && path.extname(filePath) === '.md') {
+        const fileTitle = getTitleOfBlog(filePath);
+        if (fileTitle === title) {
+          return filePath;
+        }
+      } else if (stat.isDirectory()) {
+        const foundFilePath = traverseFolder(filePath);
+        if (foundFilePath) {
+          return foundFilePath;
+        }
+      }
+    }
+
+    return null;
+  }
+
+  return traverseFolder(folderPath);
+}
+
+async function updateFileContent(filePath, newContent) {
+  try {
+    await writeFile(filePath, newContent, 'utf8');
+  } catch (error) {
+    throw error;
+  }
+}
+
+function getTitleOfBlog(filePath) {
+  try {
+    const data = fs.readFileSync(filePath, 'utf-8');
+    const lines = data.split('\n');
+
+    for (const line of lines) {
+      if (line.trim().startsWith('#')) {
+        return line.trim().replace(/^#+\s*/, '');
+      }
+    }
+    return null;
+  } catch (error) {
+    console.error(`Error reading file: ${filePath}`, error);
+    return null;
+  }
+}
--- frontend/apps/tekmonks/components/blog-list/blog-list.mjs
+++ frontend/apps/tekmonks/components/blog-list/blog-list.mjs
@@ -3,6 +3,7 @@
  * License: MIT - see enclosed license.txt file.
  */
 import {monkshu_component} from "/framework/js/monkshu_component.mjs";
+import { apimanager as apiman } from "/framework/js/apimanager.mjs"
 
 async function elementConnected(element) {
     const blogList = await(await fetch(`${APP_CONSTANTS.API_GET_BLOG_LIST}`)).json();
@@ -29,10 +30,15 @@ function findContainingListElement(element, targetTagName) {
   }
   
   function editBlog(element, id) {
-   
     const listElement = findContainingListElement(element, 'li');
     const blog = listElement.querySelectorAll(`.editor-${id}`)[0];
     blog.classList.toggle('editor-visible')
+  }
+  
+  async function saveEditedBlog(element, title){
+    const blogContainer = findContainingListElement(element, 'li').querySelectorAll('div[contenteditable="true"]')[0]
+    const apiResponse = await apiman.rest(APP_CONSTANTS.API_UPDATE_BLOG, "POST", {title: title, blog: blogContainer.innerHTML}, false, false);
+    if(apiResponse.status) alert(apiResponse.message)
     
   }
 
@@ -43,4 +49,4 @@ function register() {
 
 const trueWebComponentMode = true;	// making this false renders the component without using Shadow DOM
 
-export const blog_list = {trueWebComponentMode, register, elementConnected, editBlog}
+export const blog_list = {trueWebComponentMode, register, elementConnected, editBlog, saveEditedBlog}
