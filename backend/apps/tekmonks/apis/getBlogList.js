7ed13106a1117dcf3b48028a2186d79addd3390b
Jezrel Dave Sondia <jezrel.sondia@tekmonks.com>
2024-01-04 10:03:01 +0800

    show image preview of header image in edit modal

--- backend/apps/tekmonks/apis/getBlogList.js
+++ backend/apps/tekmonks/apis/getBlogList.js
@@ -23,6 +23,35 @@ function getAllFilesInFolder(folderPath) {
     return parts.length >= 2 ? parts[1] : null;
   }
 
+  function getImageInfo(blogFilePath) {
+    const blogDirectoryName = path.dirname(blogFilePath);
+    const nameDirHash = path.basename(path.dirname(blogFilePath));
+    const orgDirHash = path.basename(path.dirname(path.dirname(blogFilePath)));
+    const imgFolderPath = path.join(blogDirectoryName, 'img');
+    const blogFileName = path.basename(blogFilePath, path.extname(blogFilePath));
+    const blogBaseUrl = 'https://tekmonks.com/apps/tekmonks/articles/blogs.md';
+    const imageBaseUrl = `${blogBaseUrl}/${orgDirHash}/${nameDirHash}/img`;
+
+    try {
+        const imgFiles = fs.readdirSync(imgFolderPath);
+
+        for (const imgFile of imgFiles) {
+            const imgFilePath = path.join(imgFolderPath, imgFile);
+            const imgFileName = path.basename(imgFilePath, path.extname(imgFilePath));
+
+            if (imgFileName === blogFileName) {
+                const imageContent = fs.readFileSync(imgFilePath, 'utf8');
+                const imagePath = `${imageBaseUrl}/${imgFile}`;
+                return { path: imagePath, content: imageContent };
+            }
+        }
+
+        return null;
+    } catch (error) {
+        return null;
+    }
+}
+
   function traverseFolder(currentPath) {
     const files = fs.readdirSync(currentPath);
 
@@ -35,6 +64,7 @@ function getAllFilesInFolder(folderPath) {
         const lastModified = new Date(stat.mtime);
         const content = fs.readFileSync(filePath, 'utf8');
         const formattedDate = lastModified.toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: '2-digit' });
+        const imageInfo = getImageInfo(filePath);
 
         fileList.push({
           id: id,
@@ -42,7 +72,8 @@ function getAllFilesInFolder(folderPath) {
           path: filePath,
           lastModified: formattedDate,
           content: content,
-          [getLanguageFromFileName(file)]: true
+          [getLanguageFromFileName(file)]: true,
+          image: imageInfo
         });
         id += 1;
       } else if (stat.isDirectory()) {
